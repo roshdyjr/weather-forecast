ef6f4be778282e15c1567b2c3635796a62bf2c9d
roshdyjr <ahmedrush96@gmail.com>
2024-06-15 05:58:03 +0300

    changed news

--- js/news.js
+++ js/news.js
@@ -3,12 +3,24 @@ const newsApiUrl = `https://newsapi.org/v2/everything?q=weather&apiKey=${newsApi
 
 const newsContainer = document.querySelector(".news-container");
 
-
 async function fetchNews() {
     try {
-        let response = await fetch(newsApiUrl);
+        let response = await fetch(newsApiUrl, {
+            method: 'GET',
+            mode: 'cors',
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
         let newsData = await response.json();
-        displayNews(newsData.articles);
+
+        if (newsData.articles && newsData.articles.length > 0) {
+            displayNews(newsData.articles);
+        } else {
+            throw new Error("No articles found in the API response.");
+        }
     } catch (error) {
         console.error("Error fetching news:", error);
         newsContainer.innerHTML = `<p>Error fetching news. Please try again later.</p>`;
@@ -18,9 +30,7 @@ async function fetchNews() {
 function displayNews(articles) {
     let newsHTML = "";
 
-    for (let i = 0; i < articles.length; i++) {
-        let article = articles[i];
-
+    articles.forEach(article => {
         newsHTML += `
             <div class="col-lg-4 col-md-6">
                 <div class="news-card">
@@ -28,12 +38,12 @@ function displayNews(articles) {
                     <div class="news-content">
                         <h3>${article.title}</h3>
                         <p>${article.description}</p>
-                        <a href="${article.url}" target="_blank" class="btn">Read More</a>
+                        <a href="${article.url}" target="_blank" class="btn btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
         `;
-    }
+    });
 
     newsContainer.innerHTML = newsHTML;
 }
